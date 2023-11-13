8bcae23900452e565b6399abd7bc71aa29378b44
Val16eria <KStorer61@icloud.com>
2023-11-13 12:27:40 +0300

    change yaml

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-echo 'Switching to branch main'
+echo "Switching to branch main"
 git checkout master
 
-echo 'Building app...'
+echo "Building app..."
 yarn build
 
-echo 'Deploying files to server...'
+echo "Deploying files to server..."
 scp -r -P 32522 build/* root@wis-software.ru:/var/www/test
 
 echo "Done!"
